9dd214da57a3d9eade0496984b9810f007b34768
Cody Carlson <codycarlson@microsoft.com>
2024-09-05 10:13:49 -0600

    changing vars again for github provider

--- scripts/provider.tf
+++ scripts/provider.tf
@@ -11,6 +11,10 @@ terraform {
 
 provider "github" {
   owner = local.github_organization
-  app_auth {}
+  app_auth {
+    id              = var.github_app_id
+    installation_id = var.github_app_installation_id
+    pem_file        = file(env("GITHUB_APP_PEM_FILE"))
+  }
 }
 
--- scripts/variables.tf
+++ scripts/variables.tf
@@ -3,3 +3,13 @@ variable github_organization {
   type = string
   default = "ms-mfg-community"
 }
+
+variable "github_app_id" {
+  description = "The ID of the GitHub App"
+  type        = string
+}
+
+variable "github_app_installation_id" {
+  description = "The installation ID of the GitHub App"
+  type        = string
+}
