9cb68c75b7d120fac08e42f2ae6e236262687bf4
Cody Carlson <codycarlson@microsoft.com>
2024-09-04 20:44:23 -0600

    removing unnecessary variables

--- scripts/variables.tf
+++ scripts/variables.tf
@@ -1,18 +1,3 @@
-variable app_id {
-  description = "The GitHub App ID"
-  type = string
-}
-
-variable app_installation_id {
-  description = "The GitHub App Installation ID"
-  type = string
-}
-
-variable app_pem_file {
-  description = "The path to the GitHub App PEM file"
-  type = string
-}
-
 variable github_organization {
   description = "The GitHub organization to manage"
   type = string
