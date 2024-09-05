4309573ec2c0c64c1827adc786fcae5d634f44c3
Cody Carlson <codycarlson@microsoft.com>
2024-09-05 11:10:49 -0600

    updating locals to match github naming standards

--- scripts/locals.tf
+++ scripts/locals.tf
@@ -1,12 +1,12 @@
 locals {
   github_repositories = {
     repo1 = {
-      github_repository = "this repository"
+      github_repository = "tf-created-repo-1"
       repository_description = "description for this repository"
       visibility = "private"
     },
     repo2 = {
-      github_repository = "another repository"
+      github_repository = "tf-created-repo-2"
       repository_description = "description for another repository"
       visibility = "private"
     }
