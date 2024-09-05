54c1c9c02f87c5e61ca54a4bf5442fd3d2422f6d
Cody Carlson <codycarlson@microsoft.com>
2024-09-05 11:50:55 -0600

    adding team membership creation and links to repositories

--- scripts/locals.tf
+++ scripts/locals.tf
@@ -4,12 +4,27 @@ locals {
       github_repository = "tf-created-repo-1"
       repository_description = "description for this repository"
       visibility = "private"
+      owner = "tf-team1" #Setting team ownership, team must map to a team that exists in GitHub and/or is created in GitHub_Teams below
     },
     repo2 = {
       github_repository = "tf-created-repo-2"
       repository_description = "description for another repository"
       visibility = "private"
+      owner = "tf-team2"
     }
+  }
+
+  github_teams = {
+    team1 = {
+      team_name = "tf-team1"
+      description = "team1 description"
+      privacy = "closed"
+    },
+    team2 = {
+      team_name = "tf-team2"
+      description = "team2 description"
+      privacy = "closed"
+      }
   }
 
   github_organization = "ms-mfg-community"
--- scripts/main.tf
+++ scripts/main.tf
@@ -1,6 +1,23 @@
-resource "github_repository" "this" {
+# Add teams to the organization
+resource "github_team" "teams" {
+  for_each    = local.github_teams
+  name        = each.value.team_name
+  description = each.value.description
+  privacy     = each.value.privacy
+}
+
+# Add repositories to the organization
+resource "github_repository" "repositories" {
   for_each    = local.github_repositories
   name        = each.value.github_repository
   description = each.value.repository_description
   visibility  = each.value.visibility
+}
+
+# Link teams to repositories
+resource "github_team_repository" "team_repos" {
+  for_each            = local.github_repositories
+  team_id             = lookup(github_team.teams, each.value.owner).id
+  repository          = github_repository.this[each.key].name
+  permission          = "admin" # Let's set the owner for delegation
 }
